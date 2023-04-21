f759cb432fddf8a87b12599d3993af2596050773
coderain <swjh1427@gmail.com>
2023-04-21 08:57:31 +0100

    final change

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -8,9 +8,7 @@ function Footer() {
             <div className='px-8 py-3'>
                 {/* <p>This is a completely unique site</p> */}
                 <p>This clone site was developed by
-                    <a className='pl-1 text-blue-800' href='https://www.linkedin.com/in/jeremy-ashley-webdev/'>
-                        Jeremy Ashley </a>
-
+                    Code Rain
                 </p>
             </div>
 
